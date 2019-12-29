001bc49300c3dfdd2e7db9108022e6f2d90ee7b3
Shinobu Okano <okanoshinobu8@gmail.com>
2019-12-29 22:22:34 +0900

    Add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-error">
+    <div>
+      <h1>Error</h1>
+    </div>
+    <h1 v-if="error.statusCode === 404">ページが見つかりません</h1>
+    <h1 v-else>エラーです</h1>
+    <nuxt-link to="/">ホーム</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: "error",
+    props: ['error']
+  })
+</script>
+
+<style lang="scss" scoped>
+  $block: '.p-error';
+  #{$block} {
+  }
+
+</style>
