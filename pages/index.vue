6a6deaaae8a3c3548f740df379d2bd04c4515472
Shinobu Okano <okanoshinobu8@gmail.com>
2019-12-30 17:55:29 +0900

    Use static dir image

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
     <h1>Hello Nuxt!!</h1>
+    <!--  assets dir  -->
     <img src="~/assets/nuxt_logo.svg">
     <img src="~/assets/code.png" width="300px">
+    <!--  static dir  -->
+    <img src="favicon.ico">
   </div>
 </template>
 
