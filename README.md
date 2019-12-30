4b28d60a59089472c96b663ca26879521b68f0c9
Shinobu Okano <okanoshinobu8@gmail.com>
2019-12-30 17:50:05 +0900

    Add assets image

--- README.md
+++ README.md
@@ -29,6 +29,27 @@ https://typescript.nuxtjs.org/
 https://nuxtjs.org/faq/pre-processors/
 
 
+## Nuxtのデフォルトのwebpackの設定ファイル
+
+https://github.com/nuxt/nuxt.js/blob/dev/packages/webpack/src/config/base.js
+
+
+## url-loader
+
+https://github.com/webpack-contrib/url-loader
+
+> 指定した閾値よりも小さい場合に、Base64 データ URL として条件付きでファイルに埋め込むことができます。
+> これにより、小さなファイル取得のための HTTP リクエスト数を減らすことができます。
+> もし閾値よりも大きい場合は、file-loader に自動的にフォールバックします。
+
+つまり、設定いじれば、サイズの閾値を変えられるってこと
+デフォルトの閾値は1KBみたい
+以下のファイル参照すれば設定がわかる
+https://github.com/nuxt/nuxt.js/blob/dev/packages/webpack/src/config/base.js
+
+設定を変えたかったら、build.extendで変える
+https://nuxtjs.org/api/configuration-build#extend
+
 ## Build Setup
 
 ``` bash
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <h1>Hello Nuxt!!</h1>
+    <img src="~/assets/nuxt_logo.svg">
+    <img src="~/assets/code.png" width="300px">
   </div>
 </template>
 
@@ -12,27 +14,8 @@
   })
 </script>
 
-<style>
-
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
+<style lang="scss" scoped>
+  .nuxt-logo {
+    background: url("~assets/nuxt_logo.svg");
   }
 </style>
